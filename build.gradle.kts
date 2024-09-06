a9619d8eae8f62c5f5e7c5fe73ad513e761ebe0d
Дмитрий <tokarev_dmitriy95@mail.ru>
2024-09-06 18:37:15 +0300

    Update App.java and configure Gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("java")
+    application
 }
 
 group = "hexlet.code"
@@ -9,11 +9,6 @@ repositories {
     mavenCentral()
 }
 
-dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-}
-
-tasks.test {
-    useJUnitPlatform()
+application {
+    mainClass = "hexlet.code.App"
 }
