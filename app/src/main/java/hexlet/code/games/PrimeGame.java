5456674c63dc880146cbbededfeeaf50252b976b
Дмитрий <tokarev_dmitriy95@mail.ru>
2025-10-02 18:27:49 +0300

    Обновил README, добавил класс Prime, обновил класс App

---
+++ app/src/main/java/hexlet/code/games/PrimeGame.java
@@ -0,0 +1,54 @@
+package hexlet.code.games;
+
+import hexlet.code.Engine;
+import java.util.Random;
+
+public class PrimeGame {
+    private static final String GAME_DESCRIPTION = "Answer 'yes' if given number is prime. Otherwise answer 'no'.";
+    private static final int MIN_NUMBER = 2;
+    private static final int MAX_NUMBER = 100;
+    
+    public static void play() {
+        String[][] questionsAndAnswers = generateQuestionsAndAnswers();
+        Engine.runGame(GAME_DESCRIPTION, questionsAndAnswers);
+    }
+    
+    private static String[][] generateQuestionsAndAnswers() {
+        String[][] questionsAndAnswers = new String[3][2];
+        Random random = new Random();
+        
+        for (int i = 0; i < 3; i++) {
+            int number = random.nextInt(MAX_NUMBER - MIN_NUMBER + 1) + MIN_NUMBER;
+            boolean isPrime = isPrime(number);
+            String correctAnswer = isPrime ? "yes" : "no";
+            
+            questionsAndAnswers[i][0] = String.valueOf(number);
+            questionsAndAnswers[i][1] = correctAnswer;
+        }
+        
+        return questionsAndAnswers;
+    }
+    
+    private static boolean isPrime(int number) {
+        if (number < 2) {
+            return false;
+        }
+        
+        if (number == 2) {
+            return true;
+        }
+        
+        if (number % 2 == 0) {
+            return false;
+        }
+        
+        // Проверяем делители только до квадратного корня из числа
+        for (int i = 3; i * i <= number; i += 2) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+}
